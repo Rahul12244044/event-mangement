3c585cb748103402d020c065df95c7c3d0ef3ee9
Rahul@12244044 <rahuljajoria2412@gmail.com>
2025-12-06 13:16:38 +0530

    all done

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/event-management` || 'mongodb://localhost:27017/event-management', {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -13,4 +13,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB();
+module.exports = connectDB;
